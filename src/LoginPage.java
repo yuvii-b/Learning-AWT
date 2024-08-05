61309019dcce0558bf8211c11842a4cc72899e2f
yuvaraj <yuvaraj.codes@gmail.com>
2024-08-05 18:27:14 +0530

    feat: LoginPage completed

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -1,14 +1,25 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-public class LoginPage {
+public class LoginPage implements ActionListener {
     private static final int FRAME_WIDTH = 500;
     private static final int FRAME_HEIGHT = 500;
     Frame frame;
+    Label heading;
+    Label name;
+    Label email;
+    Label password;
+    TextField nameInput;
+    TextField emailInput;
+    TextField passwordInput;
+    Button login;
+    Label result;
     LoginPage(){
         //frame
-        frame = new Frame("LOGIN PAGE");
+        frame = new Frame("LOGIN");
         frame.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -16,10 +27,67 @@ public class LoginPage {
             }
         });
 
+        //labels
+        heading = new Label("LOGIN");
+        heading.setBounds(145, 50, 200, 30);
+        heading.setFont(new Font("Serif", Font.BOLD, 30));
+        name = new Label("NAME: ");
+        name.setBounds(40, 110, 100, 30);
+        name.setFont(new Font("Brush Script MT", Font.BOLD, 24));
+        email = new Label("E-MAIL ID: ");
+        email.setBounds(40, 200, 200, 30);
+        email.setFont(new Font("Brush Script MT", Font.BOLD, 24));
+        password = new Label("PASSWORD: ");
+        password.setBounds(40, 290, 200, 30);
+        password.setFont(new Font("Brush Script MT", Font.BOLD, 24));
+        result = new Label("LOGIN FAILED");
+        result.setBounds(130, 360, 300, 30);
+        result.setFont(new Font("Agency FB", Font.BOLD, 20));
+
+        //TextFields
+        nameInput = new TextField();
+        nameInput.setBounds(250, 110, 240, 30);
+        nameInput.setFont(new Font("Brush Script MT", Font.PLAIN, 20));
+        emailInput = new TextField();
+        emailInput.setBounds(250, 200, 240, 30);
+        emailInput.setFont(new Font("Brush Script MT", Font.PLAIN, 20));
+        passwordInput = new TextField();
+        passwordInput.setBounds(250, 290, 240, 30);
+        passwordInput.setFont(new Font("Brush Script MT", Font.PLAIN, 20));
+
+        //button
+        login = new Button("LOGIN");
+        login.setBounds(185, 420, 100, 30);
+        login.setFocusable(false);
+        login.addActionListener(this);
+        login.setFont(new Font("Brush Script MT", Font.PLAIN, 15));
+
+        //adding components to the frame
+        frame.add(heading);
+        frame.add(name);
+        frame.add(email);
+        frame.add(password);
+        frame.add(nameInput);
+        frame.add(emailInput);
+        frame.add(passwordInput);
+        frame.add(login);
+        frame.add(result);
+
         //frame features
         frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         frame.setLayout(null);
         frame.setResizable(false);
         frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == login){
+            String regex = "\\d+";
+            String name = nameInput.getText();
+            String email = emailInput.getText();
+            String password = passwordInput.getText();
+
+        }
     }
 }
