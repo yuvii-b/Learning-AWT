1890e12561ba532d427bca8f4a2107001d7d1dda
yuvaraj <yuvaraj.codes@gmail.com>
2024-08-07 12:26:43 +0530

    added common constants in a separate file

--- src/constants/Constants.java
+++ src/constants/Constants.java
@@ -1,2 +1,9 @@
-package constants;public class Constants {
+package constants;
+
+public class Constants {
+    // mysql credentials
+    public static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/gui_db_schema";
+    public static final String DB_USERNAME = "root";
+    public static final String DB_PASSWORD = "2806@mysql";
+    public static final String DB_TABLE_NAME = "users";
 }
